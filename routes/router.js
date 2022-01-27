af5283575fe0dd7c160fb11968b819f8acbf0726
Emile Deloof <deloofemile@gmail.com>
2022-01-27 18:25:37 +0100

    added comments

--- routes/router.js
+++ routes/router.js
@@ -7,18 +7,19 @@ router.get("/new", (req, res) => {
 });
 
 router.post("/new", (req, res) => {
+    // Read database.json
     var data = fs.readFileSync("./database/database.json");
-    var myObject = JSON.parse(data);
-    let articleName = req.body.article
-    const testObject = {
+    var myObject = JSON.parse(data); // parse buffer to json
+    let articleName = req.body.article // request article name from front-end
+    const testObject = {        // create the object to be added to the database file
         name: articleName
     }
-    myObject.push(testObject)
-    const parsedData = JSON.stringify(myObject, null, 4)
-    fs.writeFileSync("./database/database.json", parsedData, (e) => {
+    myObject.push(testObject)     // push the object into the read data array
+    const parsedData = JSON.stringify(myObject, null, 4)   // make json buffer a string
+    fs.writeFileSync("./database/database.json", parsedData, (e) => {   //write to the database file
         if(e) throw e;
     });
-    res.redirect("./confirm");
+    res.redirect("./confirm");  // if complete redirect to confirm
 })
 
 router.get("/confirm", (req, res) => {
--- server.js
+++ server.js
@@ -1,17 +1,22 @@
+// import all needed libraries, files, ...
 const express = require("express");
 const app = express();
 const PORT = 5001
 const router = require("./routes/router")
 
+// set ejs as view engine
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({extended: false}))
-app.use(express.static("styling"));
+app.use(express.static("styling")); // styling folder static so i can link to style.css
 
+// render index file
 app.get("/", (req, res) => {
     res.render("pages/index")
 })
 
+// use router
 app.use("/pages", router)
 
+//listen to port 5001 and log when done
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
