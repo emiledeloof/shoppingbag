ae714242e15e25e03d0099e572cfc1af8315ea06
Emile Deloof <deloofemile@gmail.com>
2022-02-09 13:09:57 +0100

    ready for production

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+if(process.env.NODE_ENV !== "production"){
+    require("dotenv").config();
+}
+
 // import all needed libraries, files, ...
 const express = require("express");
 const app = express();
-const PORT = 5001
+const PORT = process.env.PORT || 5001
 const router = require("./routes/router")
 const fs = require("fs")
 const methodOverride = require("method-override")
